381008582b126f67560b11ec1e683db31e577edb
Everest Paudel <paudeleverest09@gmail.com>
2025-03-27 08:36:47 +0545

    Feat: add Working component and integrate it into HomePage; update job names for consistency

--- src/LandingPage/JobCategory.tsx
+++ src/LandingPage/JobCategory.tsx
@@ -1,5 +1,6 @@
 import { Carousel } from "@mantine/carousel";
 import { jobCategory } from "../Data/Data";
+import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 
 const JobCategory = () => {
   return (
@@ -12,13 +13,16 @@ const JobCategory = () => {
         Explore diverse job opportunities tailored to your skill.Start your
         career journey today
       </div>
-<Carousel slideSize="22%" slideGap="md" loop>
+<Carousel slideSize="22%" slideGap="md" loop className="focus-visible:[&_button]:!outline-non   [&_button]:!bg-bright-sun-400 [&_button]:!border-none [&_button]:hover:opacity-75 [&_button]:opacity-0 hover:[&_button]:opacity-200"
+  nextControlIcon={<IconArrowRight  className="h-8 w-8" />}
+  previousControlIcon={<IconArrowLeft className="h-8 w-8" />}
+>
 {
   jobCategory.map((category,index)=>(
-    <Carousel.Slide key={index}>
-  <div key={index} className="flex flex-col items-center w-64">
+    <Carousel.Slide  key={index} >
+  <div key={index} className="flex flex-col items-center w-64 gap-2 border border-bright-sun-400 p-5 rounded-xl hover:cursor-pointer hover:shadow-[0_0_5px_2px_black] my-5 transition duration-300 ease-in-out !shadow-bright-sun-300">
         <div className="p-2 bg-bright-sun-300 rounded-full">
-        <img className="h-8 w-8" src={`/Category/${category.name}.png`} alt="" />
+        <img className="h-8 w-8" src={`/Category/${category.name}.png`} alt={category.name} />
         </div>
        
         <div className="text-mine-shaft-100 text-xl font-semibold">{category.name}</div>
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,6 +3,8 @@ import Header from '../Headers/Header'
 import DreamJob from '../LandingPage/DreamJob'
 import Companies from '../LandingPage/Companies'
 import JobCategory from '../LandingPage/JobCategory'
+import Working from '../LandingPage/Working'
+import Testimonial from '../LandingPage/Testimonial'
 
 
 const HomePage = () => {
@@ -12,6 +14,8 @@ const HomePage = () => {
        <DreamJob/>
        <Companies/>
        <JobCategory/>
+       <Working/>
+       <Testimonial/>
     </div>
   )
 }
